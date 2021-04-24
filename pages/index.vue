2ad6d0d93a55933c26070b130856fc4b0e3bec7e
LaylaBouteiller <layla.bouteiller@gmail.com>
2021-04-24 18:34:01 +0200

    first

---
+++ pages/index.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="container">
+    <div>
+      <Logo />
+      <h1 class="title">MZH ESPORT</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: "QuickSand-Bold";
+  display: block;
+  font-weight: bold;
+  font-size: 100px;
+  color: #651e8e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
